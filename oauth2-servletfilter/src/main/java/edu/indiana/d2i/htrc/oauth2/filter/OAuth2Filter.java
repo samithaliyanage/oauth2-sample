7621b1895fa86ed0572ec730f03cca47a7ee35f1
Samitha Liyanage <samitha.liyanage@gmail.com>
2012-11-06 10:52:08 -0500

    OAuth2 Filter to use with resource servers.

--- oauth2-servletfilter/src/main/java/edu/indiana/d2i/htrc/oauth2/filter/OAuth2Filter.java
+++ oauth2-servletfilter/src/main/java/edu/indiana/d2i/htrc/oauth2/filter/OAuth2Filter.java
@@ -21,5 +21,21 @@
 
 package edu.indiana.d2i.htrc.oauth2.filter;
 
-public class OAuth2Filter {
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class OAuth2Filter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    public void destroy() {
+
+    }
 }
--- oauth2-servletfilter/src/main/java/edu/indiana/d2i/htrc/oauth2/filter/OAuth2ServiceClient.java
+++ oauth2-servletfilter/src/main/java/edu/indiana/d2i/htrc/oauth2/filter/OAuth2ServiceClient.java
@@ -21,5 +21,54 @@
 
 package edu.indiana.d2i.htrc.oauth2.filter;
 
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.client.Options;
+import org.apache.axis2.client.ServiceClient;
+import org.apache.axis2.transport.http.HTTPConstants;
+import org.wso2.carbon.identity.oauth.keys.stub.OAuth2TokenValidationServiceStub;
+import org.wso2.carbon.identity.oauth2.dto.xsd.OAuth2TokenValidationRequestDTO;
+import org.wso2.carbon.identity.oauth2.dto.xsd.OAuth2TokenValidationResponseDTO;
+import org.wso2.carbon.utils.CarbonUtils;
+
 public class OAuth2ServiceClient {
+    private OAuth2TokenValidationServiceStub stub;
+
+    private static final int TIMEOUT_IN_MILLIS = 15 * 60 * 1000;
+    public static final String OAUTH2_PROVIDER_URL = "https://localhost:9443/services/";
+    public static final String OAUTH2_PROVIDER_USER_NAME = "admin";
+    public static final String OAUTH2_PROVIDER_PASSWORD = "admin";
+
+    /**
+     * Instantiates OAuth2TokenValidationService
+     *
+     * @param cookie For session management
+     * @param backendServerURL URL of the back end server where OAuth2TokenValidationService is
+     *            running.
+     * @param configCtx ConfigurationContext
+     * @throws org.apache.axis2.AxisFault
+     */
+    public OAuth2ServiceClient() throws AxisFault {
+        String serviceURL = OAUTH2_PROVIDER_URL + "OAuth2TokenValidationService";
+        stub = new OAuth2TokenValidationServiceStub(null, serviceURL);
+        CarbonUtils.setBasicAccessSecurityHeaders(OAUTH2_PROVIDER_USER_NAME, OAUTH2_PROVIDER_PASSWORD, true, stub._getServiceClient());
+        ServiceClient client = stub._getServiceClient();
+        Options options = client.getOptions();
+        options.setTimeOutInMilliSeconds(TIMEOUT_IN_MILLIS);
+        options.setProperty(HTTPConstants.SO_TIMEOUT, TIMEOUT_IN_MILLIS);
+        options.setProperty(HTTPConstants.CONNECTION_TIMEOUT, TIMEOUT_IN_MILLIS);
+        options.setCallTransportCleanup(true);
+        options.setManageSession(true);
+    }
+
+    /**
+     *
+     * @param params
+     * @return
+     * @throws Exception
+     */
+    public boolean validateAuthenticationRequest(OAuth2TokenValidationRequestDTO params)
+            throws Exception {
+        OAuth2TokenValidationResponseDTO resp = stub.validate(params);
+        return resp.getValid();
+    }
 }
